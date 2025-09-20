03c9447faee994808d4d663f748a85787636c0b1
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-09-21 00:10:57 +0530

    updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Programmer-DN-AI",
           from_email: form.email,
           to_email: "your-email@example.com",
           message: form.message,
